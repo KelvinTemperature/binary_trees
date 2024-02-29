26090d05c54f987857274cc03aef3472255b95ee
Kelvin <koolkt10@gmail.com>
2024-02-28 23:28:28 -0800

    1 & 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,18 +10,13 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node, *tmp;
+	binary_tree_t *new_node;
 
-	new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 	if (parent == NULL)
 		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
 
 	if (parent->left != NULL)
 	{
